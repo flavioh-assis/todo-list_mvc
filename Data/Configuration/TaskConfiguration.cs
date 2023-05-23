4b6c2e7c1ed863336f435f169dbe79a24adba1d6
Flavio Assis <flavioh.dev@gmail.com>
2023-05-23 15:42:43 -0300

    Create Data context

---
+++ Data/Configuration/TaskConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoList.Models;
+
+namespace ToDoList.Data.Configuration
+{
+    public class TaskConfiguration : IEntityTypeConfiguration<TaskModel>
+    {
+        public void Configure(EntityTypeBuilder<TaskModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Title).HasColumnType("varchar(20)");
+            builder.Property(x => x.Description).HasColumnType("varchar(150)");
+            builder.Property(x => x.Completed).HasDefaultValue(false);
+            builder.Property(x => x.CreationDate).HasDefaultValue(DateTime.Now);
+        }
+    }
+}
---
+++ Data/Context/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Data.Configuration;
+
+namespace ToDoList.Data.Context
+{
+    public class TaskContext: DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TaskConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
