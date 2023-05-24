4aea884196352087c08dfcf8170bfb5ee4f77ac1
Flavio Assis <flavioh.dev@gmail.com>
2023-05-24 17:33:13 -0300

    Rename model attribues

--- Data/Configuration/TaskConfiguration.cs
+++ Data/Configuration/TaskConfiguration.cs
@@ -4,16 +4,16 @@ using ToDoList.Models;
 
 namespace ToDoList.Data.Configuration
 {
-    public class TaskConfiguration : IEntityTypeConfiguration<TaskModel>
-    {
-        public void Configure(EntityTypeBuilder<TaskModel> builder)
-        {
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).ValueGeneratedOnAdd();
-            builder.Property(x => x.Title).HasColumnType("varchar(20)");
-            builder.Property(x => x.Description).HasColumnType("varchar(150)");
-            builder.Property(x => x.Completed).HasDefaultValue(false);
-            builder.Property(x => x.CreationDate).HasDefaultValue(DateTime.Now);
-        }
-    }
+	public class TaskConfiguration : IEntityTypeConfiguration<TaskModel>
+	{
+		public void Configure(EntityTypeBuilder<TaskModel> builder)
+		{
+			builder.HasKey(x => x.Id);
+			builder.Property(x => x.Id).ValueGeneratedOnAdd();
+			builder.Property(x => x.Title).HasColumnType("varchar(20)");
+			builder.Property(x => x.Description).HasColumnType("varchar(150)");
+			builder.Property(x => x.IsCompleted).HasDefaultValue(false);
+			builder.Property(x => x.CreatedAt).HasDefaultValue(DateTime.Now);
+		}
+	}
 }
--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -1,11 +1,11 @@
 ﻿namespace ToDoList.Models
 {
-    public class TaskModel
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public bool Completed { get; set; }
-        public DateTime CreationDate { get; set; }
-    }
+	public class TaskModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public bool IsCompleted { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
 }
