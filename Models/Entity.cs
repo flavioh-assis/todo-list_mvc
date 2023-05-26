106b099a111d6693aeeaf50775e2290c57e82199
Flavio Assis <flavioh.dev@gmail.com>
2023-05-26 15:24:48 -0300

    Create Entity

---
+++ Models/Entity.cs
@@ -0,0 +1,7 @@
+namespace ToDoList.Models
+{
+	public class Entity
+	{
+		public int Id { get; set; }
+	}
+}
--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -1,11 +1,10 @@
 ﻿namespace ToDoList.Models
 {
-	public class TaskModel
+	public class TaskModel : Entity
 	{
-		public int Id { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
-		public bool IsCompleted { get; set; }
 		public DateTime CreatedAt { get; set; }
+		public DateTime? CompletedAt { get; set; }
 	}
 }
