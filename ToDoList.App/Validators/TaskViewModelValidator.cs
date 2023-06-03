83a242237c2e32d74c3639eeaaa48e1afd04da60
Flavio Assis <flavioh.dev@gmail.com>
2023-06-02 21:25:38 -0300

    Create validator for TaskViewModel

---
+++ ToDoList.App/Validators/TaskViewModelValidator.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using ToDoList.App.ViewModels;
+
+namespace ToDoList.App.Validators;
+
+public class TaskViewModelValidator : AbstractValidator<TaskViewModel>
+{
+    public TaskViewModelValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotNull()
+            .WithMessage("Campo 'Descrição' não pode ser nulo.")
+            .MinimumLength(3)
+            .WithMessage("Campo 'Descrição' deve ter no mínimo 3 caracteres.");
+        
+        RuleFor(x => x.Description)
+            .NotNull()
+            .WithMessage("Campo 'Descrição' não pode ser nulo.");
+    }
+}
