92d4bf107021cf43566e7b6d6d141f99a4369b95
Flavio Assis <flavioh.dev@gmail.com>
2023-06-14 17:10:09 -0300

    Create task model builder

---
+++ ToDoList.Tests/Builders/TaskModelBuilder.cs
@@ -0,0 +1,68 @@
+using System;
+using ToDoList.App.Models;
+
+namespace ToDoList.Test.Builders;
+
+public class TaskModelBuilder
+{
+    private TaskModel _task = new TaskModel();
+
+    public TaskModelBuilder()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        _task = new TaskModel
+        {
+            Title = "Title",
+            Description = "Description",
+            CreatedAt = DateTime.Today
+        };
+    }
+    
+    public TaskModelBuilder WithTitle(string title)
+    {
+        _task.Title = title;
+        return this;
+    }
+
+    public TaskModelBuilder WithDescription(string description)
+    {
+        _task.Description = description;
+        return this;
+    }
+
+    public TaskModelBuilder WithCreatedAt(DateTime createdAt)
+    {
+        _task.CreatedAt = createdAt;
+        return this;
+    }
+
+    public TaskModelBuilder WithCompletedAt(DateTime? completedAt)
+    {
+        _task.CompletedAt = completedAt;
+        return this;
+    }
+
+    public TaskModelBuilder Pending()
+    {
+        _task.CompletedAt = null;
+        return this;
+    }
+
+    public TaskModelBuilder Completed()
+    {
+        _task.CompletedAt = DateTime.Now;
+        return this;
+    }
+
+    public TaskModel Build()
+    {
+        var builtTask = _task;
+        Reset();
+
+        return builtTask;
+    }
+}
