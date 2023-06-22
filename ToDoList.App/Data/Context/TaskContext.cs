4b7f7e95505c6b8ff770be224a49d196c0e3db9b
Flavio Assis <flavioh.dev@gmail.com>
2023-06-22 12:34:04 -0300

    Create task context interface

--- ToDoList.App/Data/Context/TaskContext.cs
+++ ToDoList.App/Data/Context/TaskContext.cs
@@ -4,16 +4,23 @@ using ToDoList.App.Models;
 
 namespace ToDoList.App.Data.Context
 {
-	public class TaskContext : DbContext
-	{
-		public DbSet<TaskModel> Tasks { get; set; }
+    public interface ITaskContext
+    {
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
 
-		public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+    public class TaskContext : DbContext, ITaskContext
+    {
+        public DbSet<TaskModel> Tasks { get; set; }
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			modelBuilder.ApplyConfiguration(new TaskConfiguration());
-			base.OnModelCreating(modelBuilder);
-		}
-	}
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TaskConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
 }
