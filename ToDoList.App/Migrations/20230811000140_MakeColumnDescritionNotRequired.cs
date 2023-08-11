9aa03b43ebc077dbfb106e375ccbcbc2b73a15ad
Flavio Assis <flavioh.dev@gmail.com>
2023-08-10 21:11:30 -0300

    Migration make column 'description' not required

---
+++ ToDoList.App/Migrations/20230811000140_MakeColumnDescritionNotRequired.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.App.Migrations
+{
+    public partial class MakeColumnDescritionNotRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "varchar(100)",
+                nullable: true,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "varchar(100)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true,
+                oldDefaultValue: "");
+        }
+    }
+}
