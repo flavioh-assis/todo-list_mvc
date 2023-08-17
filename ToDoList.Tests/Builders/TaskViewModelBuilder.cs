f2278172671329d4c419b4e98eee38decf70f4f4
Flavio Assis <flavioh.dev@gmail.com>
2023-08-16 21:01:05 -0300

    Refactor builder

--- ToDoList.Tests/Builders/TaskViewModelBuilder.cs
+++ ToDoList.Tests/Builders/TaskViewModelBuilder.cs
@@ -4,20 +4,16 @@ namespace ToDoList.Tests.Builders;
 
 public class TaskViewModelBuilder
 {
-    private TaskViewModel _task = new TaskViewModel();
+    private TaskViewModel _task = new();
 
-    public TaskViewModelBuilder()
-    {
-        Reset();
-    }
-
-    private void Reset()
+    public TaskViewModelBuilder NewValidTask()
     {
         _task = new TaskViewModel
         {
-            Title = "Title",
-            Description = "Description",
+            Title = "Task Title",
+            Description = "Task description",
         };
+        return this;
     }
 
     public TaskViewModelBuilder WithTitle(string title)
@@ -32,11 +28,33 @@ public class TaskViewModelBuilder
         return this;
     }
 
+    public TaskViewModelBuilder WithTitleNull()
+    {
+        _task.Title = null;
+        return this;
+    }
+
+    public TaskViewModelBuilder WithTitleTooSmall()
+    {
+        _task.Title = "T";
+        return this;
+    }
+
+    public TaskViewModelBuilder WithTitleTooLarge()
+    {
+        _task.Title = "Huge_enormous_and_invalid_title";
+        return this;
+    }
+
+    public TaskViewModelBuilder WithDescriptionTooLarge()
+    {
+        _task.Description =
+            "Considerable_large_great_huge_immense_enormous_extensive_colossal_massive_sizeable_and_invalid_description";
+        return this;
+    }
+
     public TaskViewModel Build()
     {
-        var builtTask = _task;
-        Reset();
-
-        return builtTask;
+        return _task;
     }
 }
